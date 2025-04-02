15966d3f5c14cfe8f8ab5b23e57e13ee91c700e5
NaveenAlmiya <97077116+NaveenAlmiya@users.noreply.github.com>
2025-04-02 09:03:07 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"// treat all JS code as newer version
+
+// alert(3 + 3)// we are using nodes, not browser
+
+console.log (3 
+    +
+     3)// code readability should be high 
+
+
+console.log("Hieh")
+
+let name = "Naveen"
+let age = 18
+let isloggesdIn = false
+let state;
+
+//number => 2 t power 53
+// bigint 
+// string => ""
+// bolean => true/false
+// null => standaone value
+// undefined =>
+// symble => unidque
+
+
+//object 
+
+console.log (typeof "null")
+
+
+
